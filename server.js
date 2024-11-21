e31101c706e8c9f8f2bffec835ccaea518ed5e8c
郭佳临 <guojialin1987@163.com>
2024-11-21 20:17:57 +0800

    add baidu meta

--- server.js
+++ server.js
@@ -1,78 +1,90 @@
-import fs from 'node:fs/promises'
-import express from 'express'
+import fs from "node:fs/promises";
+import express from "express";
 
 // Constants
-const isProduction = process.env.NODE_ENV === 'production'
-const port = process.env.PORT || 5175
-const base = process.env.BASE || '/'
+const isProduction = process.env.NODE_ENV === "production";
+const port = process.env.PORT || 5175;
+const base = process.env.BASE || "/";
 
 // Cached production assets
 const templateHtml = isProduction
-  ? await fs.readFile('./dist/client/index.html', 'utf-8')
-  : ''
+  ? await fs.readFile("./dist/client/index.html", "utf-8")
+  : "";
 const ssrManifest = isProduction
-  ? await fs.readFile('./dist/client/.vite/ssr-manifest.json', 'utf-8')
-  : undefined
+  ? await fs.readFile("./dist/client/.vite/ssr-manifest.json", "utf-8")
+  : undefined;
 
 // Create http server
-const app = express()
+const app = express();
 
 // Add Vite or respective production middlewares
-let vite
+let vite;
 if (!isProduction) {
-  const { createServer } = await import('vite')
+  const { createServer } = await import("vite");
   vite = await createServer({
     server: { middlewareMode: true },
-    appType: 'custom',
+    appType: "custom",
     base,
-  })
-  app.use(vite.middlewares)
+  });
+  app.use(vite.middlewares);
 } else {
-  const compression = (await import('compression')).default
-  const sirv = (await import('sirv')).default
-  app.use(compression())
-  app.use(base, sirv('./dist/client', { extensions: [] }))
+  const compression = (await import("compression")).default;
+  const sirv = (await import("sirv")).default;
+  app.use(compression());
+  app.use(base, sirv("./dist/client", { extensions: [] }));
 }
 
-// Serve HTML
-app.use('*all', async (req, res) => {
-  try {
-    const url = req.originalUrl.replace(base, '')
+// 处理以 _baidu 开头的请求
+app.use(/^\/_baidu/, (req, res) => {
+  const specifiedString = "b0c4db187467a650cc8129e5db32e340";
+  res.set({
+    "Content-Type": "text/plain",
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Expires: "0",
+  });
+  res.status(200).send(specifiedString);
+});
 
-    let template
-    let render
+// Serve HTML
+app.use("*all", async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, "");
+
+    let template;
+    let render;
     if (!isProduction) {
       // Always read fresh template in development
-      template = await fs.readFile('./index.html', 'utf-8')
-      template = await vite.transformIndexHtml(url, template)
-      render = (await vite.ssrLoadModule('/src/entry-server.ts')).render
+      template = await fs.readFile("./index.html", "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule("/src/entry-server.ts")).render;
     } else {
-      template = templateHtml
-      render = (await import('./dist/server/entry-server.js')).render
+      template = templateHtml;
+      render = (await import("./dist/server/entry-server.js")).render;
     }
 
-    const rendered = await render(url, ssrManifest)
+    const rendered = await render(url, ssrManifest);
 
     const html = template
-      .replace(`<!--app-head-->`, rendered.head ?? '')
-      .replace(`<!--app-html-->`, rendered.html ?? '')
+      .replace(`<!--app-head-->`, rendered.head ?? "")
+      .replace(`<!--app-html-->`, rendered.html ?? "");
     // 设置 Cache-Control 头，确保浏览器每次都从服务器获取最新的内容
     res.set({
-      'Content-Type': 'text/html',
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Pragma': 'no-cache',
-      'Expires': '0'
-    })
+      "Content-Type": "text/html",
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+    });
 
-    res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
+    res.status(200).set({ "Content-Type": "text/html" }).send(html);
   } catch (e) {
-    vite?.ssrFixStacktrace(e)
-    console.log(e.stack)
-    res.status(500).end(e.stack)
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
   }
-})
+});
 
 // Start http server
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`)
-})
+  console.log(`Server started at http://localhost:${port}`);
+});
