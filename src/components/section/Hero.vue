888adb7de376727feb440598d1a44b0d6e84ce5b
郭佳临 <guojialin1987@163.com>
2024-11-25 19:25:48 +0800

    change h structure

--- src/components/section/Hero.vue
+++ src/components/section/Hero.vue
@@ -1,9 +1,9 @@
 <template>
   <div w-full flex="~ row" items-start justify-center px-4>
     <div w-full max-w-screen-lg flex="~ col" items-center justify-center>
-      <h1 w-full text-10 font-bold leading-13 md:leading-11 mt-12 md:mt-25 text-center text-gray-800>
+      <h2 w-full text-10 font-bold leading-13 md:leading-11 mt-12 md:mt-25 text-center text-gray-800>
         为故去亲友创建一个纯净的纪念网站
-      </h1>
+      </h2>
       <h3 mt-6 text-gray-500>发起人生纪念，留下人生相册，邀请亲友协作</h3>
       <button mt-14 mb-6 class="btn-solid" @click="toLink('https://m.lifepage.cc/register?c=officialsite')" px-10>免费注册</button>
     </div>
